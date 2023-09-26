afc9c33d92890e1dd572f8ee681a30eb0c6f9b38
Madcodes831 <r.medina2119@gmail.com>
2023-09-25 18:38:51 -0700

    All done with mvp and part of stretch

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React from "react";
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+const Item = (props) => {
+  const { cart, setCart } = useContext(CartContext);
 
-const Item = props => {
-	return (
-		<div className="shopping-cart_item">
-			<img src={props.image} alt={`${props.title} book`} />
+  const removeItem = () => {
+    const updatedCart = cart.filter((item) => item.id !== props.id);
+    setCart(updatedCart);
+  };
 
+  return (
+    <div className="shopping-cart_item">
+      <img src={props.image} alt={`${props.title} book`} />
 
-			<div>
-				<h1>{props.title}</h1>
-				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
-			</div>
-		</div>
-	);
+      <div>
+        <h1>{props.title}</h1>
+        <p>$ {props.price}</p>
+        <button onClick={removeItem}>Remove from cart</button>
+      </div>
+    </div>
+  );
 };
 
 export default Item;
